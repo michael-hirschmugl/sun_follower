3b092371cf2263110281ca5ad8e5896fd214d620
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2023-08-09 19:31:45 +0200

    Reformating.

--- sun_follower/Core/Src/main.c
+++ sun_follower/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -22,15 +22,14 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <string.h>
+
 #include "ds3231.h"
 #include "itoa.h"
-
 
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
 
 /* USER CODE END PTD */
 
@@ -73,29 +72,29 @@ uint8_t recvd_data;
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-  #ifdef SET_TIME_ENABLE
-    ds3231_data.sec = SECONDS;
-  	ds3231_data.min = MINUTES;
-  	ds3231_data.hour = HOURS;
-  	ds3231_data.mday = DAYS;
-  	ds3231_data.mon = MONTHS;
-  	ds3231_data.year = YEARS;
-  #endif
+/* USER CODE BEGIN Init */
+#ifdef SET_TIME_ENABLE
+  ds3231_data.sec = SECONDS;
+  ds3231_data.min = MINUTES;
+  ds3231_data.hour = HOURS;
+  ds3231_data.mday = DAYS;
+  ds3231_data.mon = MONTHS;
+  ds3231_data.year = YEARS;
+#endif
 
   /* USER CODE END Init */
 
@@ -113,7 +112,7 @@ int main(void)
   MX_TIM16_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim16);
-  HAL_UART_Receive_IT(&huart1,&recvd_data,1);
+  HAL_UART_Receive_IT(&huart1, &recvd_data, 1);
 #ifdef SET_TIME_ENABLE
   Set_Time(ds3231_data, &hi2c1);
 #endif
@@ -122,8 +121,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -132,18 +130,17 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -151,40 +148,36 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
   RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
+   */
+  RCC_ClkInitStruct.ClockType =
+      RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.PeriphClockSelection =
+      RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_I2C1;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
   PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_I2C1_Init(void) {
   /* USER CODE BEGIN I2C1_Init 0 */
 
   /* USER CODE END I2C1_Init 0 */
@@ -201,38 +194,32 @@ static void MX_I2C1_Init(void)
   hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
   hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
   hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
     Error_Handler();
   }
 
   /** Configure Analogue filter
-  */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-  {
+   */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK) {
     Error_Handler();
   }
 
   /** Configure Digital filter
-  */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
-  {
+   */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
-  * @brief TIM16 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM16_Init(void)
-{
-
+ * @brief TIM16 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM16_Init(void) {
   /* USER CODE BEGIN TIM16_Init 0 */
 
   /* USER CODE END TIM16_Init 0 */
@@ -241,30 +228,26 @@ static void MX_TIM16_Init(void)
 
   /* USER CODE END TIM16_Init 1 */
   htim16.Instance = TIM16;
-  htim16.Init.Prescaler = 48000-1;
+  htim16.Init.Prescaler = 48000 - 1;
   htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim16.Init.Period = 1000-1;
+  htim16.Init.Period = 1000 - 1;
   htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim16.Init.RepetitionCounter = 0;
   htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
-  {
+  if (HAL_TIM_Base_Init(&htim16) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN TIM16_Init 2 */
 
   /* USER CODE END TIM16_Init 2 */
-
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART1_UART_Init(void)
-{
-
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
   /* USER CODE BEGIN USART1_Init 0 */
 
   /* USER CODE END USART1_Init 0 */
@@ -282,26 +265,23 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
+  if (HAL_UART_Init(&huart1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOF_CLK_ENABLE();
@@ -309,13 +289,16 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, USR_TEMP_SENS_Pin|USR_LED_GREEN_Pin|USR_RLY_4_Pin|USR_RLY_3_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(
+      GPIOB,
+      USR_TEMP_SENS_Pin | USR_LED_GREEN_Pin | USR_RLY_4_Pin | USR_RLY_3_Pin,
+      GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin|USR_RLY_2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin | USR_RLY_2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : USR_BTN_2_Pin USR_BTN_3_Pin */
-  GPIO_InitStruct.Pin = USR_BTN_2_Pin|USR_BTN_3_Pin;
+  GPIO_InitStruct.Pin = USR_BTN_2_Pin | USR_BTN_3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -328,7 +311,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(USR_TEMP_SENS_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : USR_RLY_1_Pin USR_RLY_2_Pin */
-  GPIO_InitStruct.Pin = USR_RLY_1_Pin|USR_RLY_2_Pin;
+  GPIO_InitStruct.Pin = USR_RLY_1_Pin | USR_RLY_2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -341,7 +324,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(USR_BTN_1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : USR_LED_GREEN_Pin USR_RLY_4_Pin USR_RLY_3_Pin */
-  GPIO_InitStruct.Pin = USR_LED_GREEN_Pin|USR_RLY_4_Pin|USR_RLY_3_Pin;
+  GPIO_InitStruct.Pin = USR_LED_GREEN_Pin | USR_RLY_4_Pin | USR_RLY_3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -360,138 +343,122 @@ static void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI4_15_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-  HAL_GPIO_WritePin(GPIOB, USR_RLY_4_Pin|USR_RLY_3_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin|USR_RLY_2_Pin, GPIO_PIN_SET);
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  HAL_GPIO_WritePin(GPIOB, USR_RLY_4_Pin | USR_RLY_3_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin | USR_RLY_2_Pin, GPIO_PIN_SET);
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-	if(htim == &htim16)
-	{
-		HAL_GPIO_TogglePin(GPIOB, USR_LED_GREEN_Pin);
-		Get_Time(&ds3231_data, &hi2c1);
-	}
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
+  if (htim == &htim16) {
+    HAL_GPIO_TogglePin(GPIOB, USR_LED_GREEN_Pin);
+    Get_Time(&ds3231_data, &hi2c1);
+  }
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-	if(GPIO_Pin == USR_BTN_1_Pin)
-	{
-		switch(HAL_GPIO_ReadPin(USR_BTN_1_GPIO_Port, USR_BTN_1_Pin))
-		{
-		case GPIO_PIN_RESET:
-			HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin, GPIO_PIN_RESET);
-			break;
-		case GPIO_PIN_SET:
-			HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin, GPIO_PIN_SET);
-			break;
-		default:
-			break;
-		}
-	}
-	if(GPIO_Pin == USR_BTN_2_Pin)
-	{
-		switch(HAL_GPIO_ReadPin(USR_BTN_2_GPIO_Port, USR_BTN_2_Pin))
-		{
-		case GPIO_PIN_RESET:
-			HAL_GPIO_WritePin(GPIOA, USR_RLY_2_Pin, GPIO_PIN_RESET);
-			break;
-		case GPIO_PIN_SET:
-			HAL_GPIO_WritePin(GPIOA, USR_RLY_2_Pin, GPIO_PIN_SET);
-			break;
-		default:
-			break;
-		}
-	}
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+  if (GPIO_Pin == USR_BTN_1_Pin) {
+    switch (HAL_GPIO_ReadPin(USR_BTN_1_GPIO_Port, USR_BTN_1_Pin)) {
+      case GPIO_PIN_RESET:
+        HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin, GPIO_PIN_RESET);
+        break;
+      case GPIO_PIN_SET:
+        HAL_GPIO_WritePin(GPIOA, USR_RLY_1_Pin, GPIO_PIN_SET);
+        break;
+      default:
+        break;
+    }
+  }
+  if (GPIO_Pin == USR_BTN_2_Pin) {
+    switch (HAL_GPIO_ReadPin(USR_BTN_2_GPIO_Port, USR_BTN_2_Pin)) {
+      case GPIO_PIN_RESET:
+        HAL_GPIO_WritePin(GPIOA, USR_RLY_2_Pin, GPIO_PIN_RESET);
+        break;
+      case GPIO_PIN_SET:
+        HAL_GPIO_WritePin(GPIOA, USR_RLY_2_Pin, GPIO_PIN_SET);
+        break;
+      default:
+        break;
+    }
+  }
 }
 
-void USART1_IRQHandler(void)
-{
+void USART1_IRQHandler(void) {
+  char msg[15];
+  HAL_UART_IRQHandler(&huart1);
 
-	char msg[15];
-	HAL_UART_IRQHandler(&huart1);
-
-	switch(recvd_data)
-	{
+  switch (recvd_data) {
     case 'l':
-    	HAL_GPIO_TogglePin(GPIOB, USR_LED_GREEN_Pin);
-	    break;
-	case 't':
-		HAL_UART_Transmit(&huart1,(uint8_t*)"time:\n\r",7,1000);
-		if(Get_Time(&ds3231_data, &hi2c1) == 0xFF)
-		{
-			HAL_UART_Transmit(&huart1,(uint8_t*)"error!\n\r",8,1000);
-		}
-		else
-		{
-			memset(msg,0,15);
-			itoa(ds3231_data.hour,msg,10);
-			HAL_UART_Transmit(&huart1,(uint8_t*)msg,sizeof(msg),1000);
-			HAL_UART_Transmit(&huart1,(uint8_t*)":",1,1000);
-			memset(msg,0,15);
-			itoa(ds3231_data.min,msg,10);
-			HAL_UART_Transmit(&huart1,(uint8_t*)msg,sizeof(msg),1000);
-			HAL_UART_Transmit(&huart1,(uint8_t*)":",1,1000);
-			memset(msg,0,15);
-			itoa(ds3231_data.sec,msg,10);
-			HAL_UART_Transmit(&huart1,(uint8_t*)msg,sizeof(msg),1000);
-			HAL_UART_Transmit(&huart1,(uint8_t*)"\n\r",2,1000);
-		}
-		break;
-	case '1':
-		HAL_GPIO_TogglePin(GPIOA, USR_RLY_1_Pin);
-		break;
-	case '2':
-		HAL_GPIO_TogglePin(GPIOA, USR_RLY_2_Pin);
-		break;
-	case '3':
-		HAL_GPIO_TogglePin(GPIOB, USR_RLY_3_Pin);
-		break;
-	case '4':
-		HAL_GPIO_TogglePin(GPIOB, USR_RLY_4_Pin);
-		break;
-	default:
-		break;
-	}
+      HAL_GPIO_TogglePin(GPIOB, USR_LED_GREEN_Pin);
+      break;
+    case 't':
+      HAL_UART_Transmit(&huart1, (uint8_t*)"time:\n\r", 7, 1000);
+      if (Get_Time(&ds3231_data, &hi2c1) == 0xFF) {
+        HAL_UART_Transmit(&huart1, (uint8_t*)"error!\n\r", 8, 1000);
+      } else {
+        memset(msg, 0, 15);
+        itoa(ds3231_data.hour, msg, 10);
+        HAL_UART_Transmit(&huart1, (uint8_t*)msg, sizeof(msg), 1000);
+        HAL_UART_Transmit(&huart1, (uint8_t*)":", 1, 1000);
+        memset(msg, 0, 15);
+        itoa(ds3231_data.min, msg, 10);
+        HAL_UART_Transmit(&huart1, (uint8_t*)msg, sizeof(msg), 1000);
+        HAL_UART_Transmit(&huart1, (uint8_t*)":", 1, 1000);
+        memset(msg, 0, 15);
+        itoa(ds3231_data.sec, msg, 10);
+        HAL_UART_Transmit(&huart1, (uint8_t*)msg, sizeof(msg), 1000);
+        HAL_UART_Transmit(&huart1, (uint8_t*)"\n\r", 2, 1000);
+      }
+      break;
+    case '1':
+      HAL_GPIO_TogglePin(GPIOA, USR_RLY_1_Pin);
+      break;
+    case '2':
+      HAL_GPIO_TogglePin(GPIOA, USR_RLY_2_Pin);
+      break;
+    case '3':
+      HAL_GPIO_TogglePin(GPIOB, USR_RLY_3_Pin);
+      break;
+    case '4':
+      HAL_GPIO_TogglePin(GPIOB, USR_RLY_4_Pin);
+      break;
+    default:
+      break;
+  }
 
-	HAL_UART_Receive_IT(&huart1,&recvd_data,1);
-
+  HAL_UART_Receive_IT(&huart1, &recvd_data, 1);
 }
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- sun_follower/Debug/Core/Src/main.d
+++ sun_follower/Debug/Core/Src/main.d
@@ -28,7 +28,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h \
- ../Core/Src/ds3231.h ../Drivers/CMSIS/Include/itoa.h
+ ../Core/Src/ds3231.h ../Core/Src/itoa.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
 ../Core/Inc/stm32f0xx_hal_conf.h:
@@ -60,4 +60,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
 ../Core/Src/ds3231.h:
-../Drivers/CMSIS/Include/itoa.h:
+../Core/Src/itoa.h:
